2f9dd69717b100ce22de2b7f02d7497f4e452a5d
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2020-01-27 16:25:20 -0500

    adding support for rest of data links fields
    needs unit test code, code is close but probably correct

--- adsdata/process.py
+++ adsdata/process.py
@@ -27,9 +27,19 @@ data_files['reference'] = {'path': 'config/links/reference/all.links'}
 data_files['relevance'] = {'path': 'config/links/relevance/docmetrics.tab'}
 data_files['simbad'] = {'path': 'config/links/simbad/simbad_objects.tab'}
 
-data_files['electr'] = {'path': 'config/links/eprint_html/all.links'}
-#data_files['pub_html'] = {'path': 'config/links/electr/all.links'}
-
+data_files['pub_html'] = {'path': 'config/links/electr/all.links'}
+data_files['eprint_html'] = {'path': 'config/links/eprint_html/all.links'}
+data_files['pub_pdf'] = {'path': 'config/links/pub_pdf/all.links'}
+data_files['ads_pdf'] = {'path': 'config/links/ads_pdf/all.links'}
+data_files['eprint_pdf'] = {'path': 'config/links/eprint_pdf/all.links'}
+data_files['author_html'] = {'path': 'config/links/author_html/all.links'}
+data_files['author_pdf'] = {'path': 'config/links/author_pdf/all.links'}
+data_files['ads_pdf'] = {'path': 'config/links/ads_scan/all.links'}
+data_files['associated'] = {'path': 'config/links/associated/all.links'}
+data_files['presentation'] = {'path': 'config/links/video/all.links'}
+data_files['librarycatalog'] = {'path': 'config/links/library/all.links'}
+data_files['inspire'] = {'path': 'config/links/spires/all.links'}
+# data_files['toc'] = {'path': 'config/links/toc/all.links'}  # I don't see how data in this file affect the nonbib record
 
 
 #data_files['data_link'] = {'path': 'facet_datasources/datasources.links'}
@@ -60,8 +70,9 @@ def convert(d):
     most values are read into an array, 
     the exception is boolean membership like refereed"""
     ret = {}
-    esources = []
+    esources = set()
     links_data = []
+    properties = set()
     for k in nonbib_keys:  # should we iterate over keys in protobuf instead?
         if k == 'canonical':
             ret['bibcode'] = d[k]
@@ -75,13 +86,39 @@ def convert(d):
             # if there is no value, provide boolean default
             ret[k] = False
             print 'changing private to false: {}'.format(ret)
-        elif k in ('pub_html'):
-            if k is 'pub_html' and d[k]:
-                esources.append(k.upper_case())
-                t = k.split('_')[1].lower_case()
-                links_data.append({'access': '', 'instances': '', 'title': '', 'type': t , 'url': d[k]}
-        else:
-            ret[k] = d[k]
+        elif k in ('associated') and d[k]:
+            # sample:
+            #    1825AN......4..241B     1825AN......4..241B Main Paper
+            #    1825AN......4..241B     2010AN....331..852K Translation
+            urls = []
+            titles = []
+            for current in d[k]:
+                urls.append(current.split('\t')[0][:19])
+                titles.append(current.split('\t')[0][20:])
+            links_data.append({'title': ','.join(titles), 'url': ','.join(urls),  # do we need quotes around array elements?
+                               'link_type': k.upper(), 'link_sub_type': 'NA', 'item_count': len(links_data), 'title': []})
+            properties.add('ESOURCE')
+        elif k in ('presentation', 'librarycatalog', 'inspire') and d[k]:
+            # sample:
+            #   1997kbls.confE..10C	http://online.kitp.ucsb.edu/online/bblunch/carroll/
+            links_data.append({'link_type': k.upper(), 'link_sub_type': 'NA', 'url': [d[k]], 'item_count': len(links_data), 'title': []})
+            properties.add(k.upper())
+
+        elif k in ('pub_html', 'eprint_html', 'pub_pdf', 'ads_pdf', 'eprint_pdf', 'author_html', 'author_pdf', 'ads_pdf'):
+            if d[k]:
+                esources.append(k.upper())
+                t = k.split('_')[1].lower()
+                links_data.append({'item_count': len(links_data), 'title': [],
+                                   'link_type': 'ESOURCE', 'link_sub_type': t.upper(), 'url': [d[k]]})
+            if 'author' in k:
+                properties.add('AUTHOR_OPENACCESS')
+            if 'ads' in k:
+                properties.add('ADS_OPENACCESS')
+                esources.add('ADS_PDF')
+                esources.add('ADS_SCAN')
+            properties.add('ESOURCE')
+            properties.add('OPENACCESS')
+
         if k in ('citation', 'reads'):
             count = 0
             if d[k]:
@@ -90,6 +127,10 @@ def convert(d):
             if k == 'reads':
                 tmp = 'read_count'  # not reads_count
             ret[tmp] = count
+
+    ret['property'] = list(properties)
+    ret['esource'] = list(esources)
+    ret['data_links_rows'] = links_data
         #if k == 'author':
         #    ret['author_count'] = 0
         #    if d[k]:
